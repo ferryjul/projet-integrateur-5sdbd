0da62ee40053d43d22055ac43f95ca68b26feb65
VM <vm@vm.fr>
2019-11-21 16:05:33 +0100

    updates storageMS

--- storageMS/storageMS.py
+++ storageMS/storageMS.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
-from flask.ext.jsonpify import jsonify
+from flask_jsonpify import jsonify
 import MySQLdb
 import csv
 import requests
@@ -20,12 +20,14 @@ column_names = '(tripduration, starttime, stoptime, start_station_id, \
 class Dataset(Resource):
     def get(self, dataset_address):
         # Open BDD
+        print("Opening BDD")
         mydb = MySQLdb.connect(host='localhost',
             user='root',
             passwd='useruser',
             db='mydb')
         cursor = mydb.cursor()
         # Download and save file
+        print("Downloading file")
         file = requests.get(dataset_address, stream=True)
         dump = file.raw
         location = os.path.relpath("./")
@@ -33,37 +35,65 @@ class Dataset(Resource):
             shutil.copyfileobj(dump, location)
         del dump
         # Load csv in database
-        csv_data = csv.reader(file('file_tmp.csv'))
+        print("Loading dataset")
+        csv_data = csv.reader(open("/home/user/tests/JC-201509-citibike-tripdata.csv"))
+        first = True
         for row in csv_data:
-            cursor.execute('INSERT INTO ' + table_name + column_names \
+            # Allow blank cells
+            for i in range(len(row)):
+                if row[i] == '':
+                    row[i] = None
+            if first:
+                first = False
+            else:
+                cursor.execute('INSERT INTO data(trip_duration, start_time, stop_time,    start_station_id, \
+                start_station_name, start_station_latitude, start_station_longitude, \
+                end_station_id, end_station_name, end_station_latitude, end_station_longitude, \
+                bike_id, user_type, birth_year, gender )' \
                 'VALUES(%s, %s, %s, %s, \
                 %s, %s, %s, \
                 %s, %s, %s, %s, \
                 %s, %s, %s, %s)', row)
         # Close BDD
+        print("Closing BDD")
         mydb.commit()
         cursor.close()
         # Delete temporary file
+        print("Deleting temporary files")
         os.remove('file_tmp.csv')
         return "Done"
 
 class DatasetLocal(Resource):
     def get(self, dataset_address):
         # Open BDD
+        print("Opening BDD")
         mydb = MySQLdb.connect(host='localhost',
             user='root',
             passwd='useruser',
             db='mydb')
         cursor = mydb.cursor()
         # Load csv in database
-        csv_data = csv.reader(file(dataset_address))
+        print("Loading dataset")
+        csv_data = csv.reader(open("/home/user/tests/JC-201509-citibike-tripdata.csv"))
+        first = True
         for row in csv_data:
-            cursor.execute('INSERT INTO ' + table_name + column_names \
+            # Allow blank cells
+            for i in range(len(row)):
+                if row[i] == '':
+                    row[i] = None
+            if first:
+                first = False
+            else:
+                cursor.execute('INSERT INTO data(trip_duration, start_time, stop_time,    start_station_id, \
+                start_station_name, start_station_latitude, start_station_longitude, \
+                end_station_id, end_station_name, end_station_latitude, end_station_longitude, \
+                bike_id, user_type, birth_year, gender )' \
                 'VALUES(%s, %s, %s, %s, \
                 %s, %s, %s, \
                 %s, %s, %s, %s, \
                 %s, %s, %s, %s)', row)
         # Close BDD
+        print("Closing BDD")
         mydb.commit()
         cursor.close()
         return "Done"
@@ -77,4 +107,4 @@ api.add_resource(DatasetLocal, '/add-local-dataset/<dataset_address>') # Route_2
 
 
 if __name__ == '__main__':
-     app.run(port='80')
+     app.run(port='80', host='192.168.1.5')
