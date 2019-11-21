90784e84bc8e602961b2b13b3e60d820295034de
Jerome Bravais <jerome@vpnssl-memberinsa40-1.insa-toulouse.fr>
2019-11-21 17:36:23 +0100

    modif code python et ajout des useful commands

--- storageMS/storageMS.py
+++ storageMS/storageMS.py
@@ -72,7 +72,7 @@ class Dataset(Resource):
 
 class DatasetLocal(Resource):
     def get(self, dataset_address):
-        print(request.form['src'])
+        print(dataset_address)
         # Open BDD
         print("Opening BDD")
         mydb = MySQLdb.connect(host='localhost',
@@ -112,10 +112,10 @@ class DatasetLocal(Resource):
         return "Done"
 
 # Give URL to dataset
-api.add_resource(Dataset, '/add-distant-dataset') # Route_1
+api.add_resource(Dataset, '/add-distant-dataset/<dataset_address>') # Route_1
 
 # Give absolute path to dataset
-api.add_resource(DatasetLocal, '/add-local-dataset') # Route_2
+api.add_resource(DatasetLocal, '/add-local-dataset/<dataset_address>') # Route_2
 
 
 
