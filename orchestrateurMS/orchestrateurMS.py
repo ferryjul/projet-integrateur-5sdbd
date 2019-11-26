a5b2d93de66992519ff8eaa41754fbcd9d699eb9
Jerome Bravais <jerome@vpnssl-memberinsa40-1.insa-toulouse.fr>
2019-11-26 01:27:22 +0100

    Ajout orchestrateur

--- orchestrateurMS/orchestrateurMS.py
+++ orchestrateurMS/orchestrateurMS.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, redirect
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
 from json import dumps
 from flask_jsonpify import jsonify
 import requests
