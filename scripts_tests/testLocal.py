bbb35883bbd722830b79832145f985ef20797df6
Julien Ferry <ferry@insat.insa-toulouse.fr>
2019-11-27 15:32:17 +0100

    Improves display tests

--- scripts_tests/testLocal.py
+++ scripts_tests/testLocal.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os.path
+import pickle
 
 # Print iterations progress
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
@@ -23,14 +25,72 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     # Print New Line on Complete
     if iteration == total: 
         print()
+        
 
-#input_file = "JC-201509-citibike-tripdata.csv"
+input_file = "JC-201509-citibike-tripdata.csv"
 #input_file = "201306-citibike-tripdata.csv"
-input_file = "2014-02 - Citi Bike trip data.csv"
+#input_file = "2014-02 - Citi Bike trip data.csv"
 data = pd.read_csv(input_file) 
+max_radius = 20
 # Initial call to print 0% progress
 l = len(data.iloc[:,4])
-printProgressBar(0, l, prefix = 'Discovering stations:', suffix = 'Complete', length = 50)
+
+station_dict = dict()
+if os.path.exists("./" + input_file + ".dump"):
+    with open(input_file + ".dump", 'rb') as handle:
+        station_dict = pickle.load(handle)
+    print("Retrieved dictionnary from local directory.")
+else:
+    printProgressBar(0, l, prefix = 'Discovering stations:', suffix = 'Complete', length = 50)
+    for index in range(len(data.iloc[:,4])):
+        if not (data.iloc[:,4][index]) in station_dict:
+            station_dict[data.iloc[:,4][index]] = [data.iloc[:,3][index], data.iloc[:,5][index], data.iloc[:,6][index], 1, 0]
+        else:
+            currNbDepartures = station_dict[data.iloc[:,4][index]][3]
+            currNbArrivals = station_dict[data.iloc[:,4][index]][4]
+            station_dict[data.iloc[:,4][index]] = [data.iloc[:,3][index], data.iloc[:,5][index], data.iloc[:,6][index], currNbDepartures+1, currNbArrivals]
+        if not (data.iloc[:,8][index]) in station_dict:
+            station_dict[data.iloc[:,8][index]] = [data.iloc[:,7][index], data.iloc[:,9][index], data.iloc[:,10][index], 0, 1]
+        else:
+            currNbDepartures = station_dict[data.iloc[:,8][index]][3]
+            currNbArrivals = station_dict[data.iloc[:,8][index]][4]
+            station_dict[data.iloc[:,8][index]] = [data.iloc[:,7][index], data.iloc[:,9][index], data.iloc[:,10][index], currNbDepartures, currNbArrivals+1]
+        printProgressBar(index + 1, l, prefix = 'Discovering stations:', suffix = 'Complete', length = 50)
+    with open(input_file + ".dump", 'wb') as handle:
+        pickle.dump(station_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print("Dumped computed dictionnary.")
+
+#print(stations)
+latitudes = [station_dict[i][1] for i in station_dict]
+longitudes = [station_dict[i][2] for i in station_dict]
+departures = [station_dict[i][3] for i in station_dict]
+arrivals = [station_dict[i][4] for i in station_dict]
+
+traffic = departures
+trafficSize = []
+colorPoints = []
+max_traffic = max(traffic)
+print("max departures = ", max_traffic)
+ij = 0
+for s in station_dict:
+    trafficSize.append((traffic[ij]/max_traffic)*max_radius)
+    if 0.8*max_traffic < (traffic[ij]):
+        colorPoints.append('red')
+    else:
+        colorPoints.append('blue')
+    ij = ij + 1
+
+plt.scatter(latitudes, longitudes, s=traffic, c = colorPoints)
+print("Processed ", l, "lines of data.")
+print("Found ", len(station_dict), " stations.")
+
+
+'''# To print names uncomment
+for i, txt in enumerate(station_names):
+    plt.annotate(txt, (latitudes[i], longitudes[i]))'''
+plt.show()
+
+'''
 stations = []
 station_names = []
 for index in range(len(data.iloc[:,4])):
@@ -44,34 +104,16 @@ for index in range(len(data.iloc[:,4])):
 #print(stations)
 latitudes = [stations[i][2] for i in range(len(stations))]
 longitudes = [stations[i][3] for i in range(len(stations))]
-traffic = []
-#for s in station_names:
-#    traffic.append(1)
-plt.scatter(latitudes, longitudes) #, s=traffic)
+traffic = [10, 5, 5, 6, 1]
+max_traffic = max(traffic)
+print("max = ", max_traffic)
+ij = 0
+for s in station_names:
+    traffic.append((traffic[ij]/max_traffic)*max_radius)
+plt.scatter(latitudes, longitudes , s=traffic)
 print("Found ", len(station_names), " stations.")
 
 # To print names uncomment
-'''for i, txt in enumerate(station_names):
-    plt.annotate(txt, (latitudes[i], longitudes[i]))'''
-plt.show()
-
-
-
-
-
-'''
-for index in range(len(data['Start Station Name'])):
-    if not (data['Start Station Name'][index]) in station_names:
-        station_names.append(data['Start Station Name'][index])
-        stations.append([data['Start Station ID'][index], data['Start Station Name'][index], data['Start Station Latitude'][index], data['Start Station Longitude'][index]])
-    if not (data['End Station Name'][index]) in station_names:
-        station_names.append(data['End Station Name'][index])
-        stations.append([data['End Station ID'][index], data['End Station Name'][index], data['End Station Latitude'][index], data['End Station Longitude'][index]])
-#print(stations)
-latitudes = [stations[i][2] for i in range(len(stations))]
-longitudes = [stations[i][3] for i in range(len(stations))]
-plt.scatter(latitudes, longitudes)
-print("Found ", len(station_names), " stations.")
 for i, txt in enumerate(station_names):
     plt.annotate(txt, (latitudes[i], longitudes[i]))
 plt.show()
