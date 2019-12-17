88b2c7f32c5d65848c8a704470b12746ba11296f
Jerome Bravais <jerome@macbook-4.local>
2019-12-17 15:35:57 +0100

    modif cassandra

--- storageMS/storageMS.py
+++ storageMS/storageMS.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
 from json import dumps
 from flask_jsonpify import jsonify
 from cassandra.cluster import Cluster
