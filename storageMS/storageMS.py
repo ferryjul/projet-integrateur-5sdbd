ba890287339d9c35fb7baf36f5ad2caba3de8b86
Jerome Bravais <jerome@vpnssl-memberinsa40-4.insa-toulouse.fr>
2019-12-21 20:11:52 +0100

    fix ip flotantes orchestrateur

--- storageMS/storageMS.py
+++ storageMS/storageMS.py
@@ -83,12 +83,6 @@ def update_table(addr):
                 pass
 
             try:
-                row[2] = datetime.datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S.%f')
-                row[3] = datetime.datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S.%f')
-            except:
-                pass
-
-            try:
                 rq = "INSERT INTO data(trip_id, trip_duration, start_time, stop_time, start_station_id, \
                 start_station_name, start_station_latitude, start_station_longitude, \
                 end_station_id, end_station_name, end_station_latitude, end_station_longitude, \
