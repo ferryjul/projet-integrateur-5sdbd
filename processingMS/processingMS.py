a2b39bc027d21cc0c26105360bea6902c1b19595
Ptiteigne <j-640@hotmail.fr>
2020-01-08 10:39:39 +0100

    Add request to get a corrected stations status json

--- processingMS/processingMS.py
+++ processingMS/processingMS.py
@@ -26,6 +26,33 @@ class Test(Resource):
     def get(self, data):
         return "Done"
 
+class StationsStatusJSONQuery(Resource):
+	# Make and return a realtime stations status json indexed by station_id
+	@staticmethod
+	def get_correct_stations_status_json():
+		r = requests.get(url='https://gbfs.citibikenyc.com/gbfs/en/station_status.json')
+		original_stations_status_json = r.json()
+
+		# Instantiate and build a stations status dict indexed by station_id
+		sorted_stations_status = {}	
+		for i in range(len(original_stations_status_json["data"]["stations"])):
+		    sorted_stations_status[original_stations_status_json["data"]["stations"][i]["station_id"]] = original_stations_status_json["data"]["stations"][i]
+		
+		# Corrected json as a dictionary
+		corrected_stations_status_dict = {
+		    'last_updated' : original_stations_status_json['last_updated'],
+		    'stations' : sorted_stations_status,
+		}
+
+		# JSONify then return the corrected_station_status
+		return jsonify(corrected_stations_status_dict)
+
+	def get(self):
+		return StationsStatusJSONQuery.get_correct_stations_status_json()
+
+# Give GET request to get a corrected version of the realtime station status json
+api.add_resource(StationsStatusJSONQuery, '/stations_current_status.json')
+
 # Ping method for the orchestrateurMS
 api.add_resource(Ping, '/ping')
 
